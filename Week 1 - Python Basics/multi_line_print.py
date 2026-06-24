f713dccfa1fcb739736709f4364459e6a797ef65
Niharika Chandak <niharikachandak.ecell@gmail.com>
2026-06-24 17:48:06 +0530

    Initial project upload

---
+++ Week 1 - Python Basics/multi_line_print.py
@@ -0,0 +1,14 @@
+# an efficient way to print multiple lines together
+
+print(''''
+      The way a crow
+      Shook down on me
+      The dust of snow
+      From a hemlock tree
+      Has given my heart
+      A change of mood
+      And saved some part
+      Of a day I had rued
+      ''')
+
+# you could have also placed the text between these quotes """ """
